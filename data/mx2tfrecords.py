e30c17e37c4e1e2d56932359b9bfb6007d773cee
bhneo <bhneo@126.com>
2019-06-11 17:57:11 +0800

    update

--- data/mx2tfrecords.py
+++ data/mx2tfrecords.py
@@ -6,6 +6,7 @@ import numpy as np
 import cv2
 import tensorflow as tf
 import os
+import matplotlib.pyplot as plt
 
 
 def parse_args():
@@ -13,11 +14,11 @@ def parse_args():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         description='data path information'
     )
-    parser.add_argument('--bin_path', default='../datasets/faces_ms1m_112x112/train.rec', type=str,
+    parser.add_argument('--bin_path', default='train.rec', type=str,
                         help='path to the binary image file')
-    parser.add_argument('--idx_path', default='../datasets/faces_ms1m_112x112/train.idx', type=str,
+    parser.add_argument('--idx_path', default='train.idx', type=str,
                         help='path to the image index path')
-    parser.add_argument('--tfrecords_file_path', default='../datasets/tfrecords', type=str,
+    parser.add_argument('--tfrecords_file_path', default='', type=str,
                         help='path to the output of tfrecords file path')
     args = parser.parse_args()
     return args
@@ -46,7 +47,7 @@ def mx2tfrecords_old(imgidx, imgrec, args):
 
 
 def mx2tfrecords(imgidx, imgrec, args):
-    output_path = os.path.join(args.tfrecords_file_path, 'tran.tfrecords')
+    output_path = os.path.join(args.tfrecords_file_path, 'train.tfrecords')
     writer = tf.python_io.TFRecordWriter(output_path)
     for i in imgidx:
         img_info = imgrec.read_idx(i)
@@ -69,8 +70,8 @@ def parse_function(example_proto):
     # You can do more image distortion here for training data
     img = tf.image.decode_jpeg(features['image_raw'])
     img = tf.reshape(img, shape=(112, 112, 3))
-    r, g, b = tf.split(img, num_or_size_splits=3, axis=-1)
-    img = tf.concat([b, g, r], axis=-1)
+    # r, g, b = tf.split(img, num_or_size_splits=3, axis=-1)
+    # img = tf.concat([b, g, r], axis=-1)
     img = tf.cast(img, dtype=tf.float32)
     img = tf.subtract(img, 127.5)
     img = tf.multiply(img,  0.0078125)
@@ -80,30 +81,33 @@ def parse_function(example_proto):
 
 
 if __name__ == '__main__':
-    # # define parameters
+    # define parameters
     # id2range = {}
     # data_shape = (3, 112, 112)
-    args = parse_args()
+    args = parse_args()#############################################################################
     # imgrec = mx.recordio.MXIndexedRecordIO(args.idx_path, args.bin_path, 'r')
     # s = imgrec.read_idx(0)
     # header, _ = mx.recordio.unpack(s)
     # print(header.label)
     # imgidx = list(range(1, int(header.label[0])))
-    # seq_identity = range(int(header.label[0]), int(header.label[1]))
-    # for identity in seq_identity:
-    #     s = imgrec.read_idx(identity)
-    #     header, _ = mx.recordio.unpack(s)
-    #     a, b = int(header.label[0]), int(header.label[1])
-    #     id2range[identity] = (a, b)
-    # print('id2range', len(id2range))
-
+    # # seq_identity = range(int(header.label[0]), int(header.label[1]))
+    # # for identity in seq_identity:
+    # #     s = imgrec.read_idx(identity)
+    # #     header, _ = mx.recordio.unpack(s)
+    # #     a, b = int(header.label[0]), int(header.label[1])
+    # #     id2range[identity] = (a, b)
+    # # print('id2range', len(id2range))
+    #
     # # generate tfrecords
     # mx2tfrecords(imgidx, imgrec, args)
+
+
+
 
     config = tf.ConfigProto(allow_soft_placement=True)
     sess = tf.Session(config=config)
     # training datasets api config
-    tfrecords_f = os.path.join(args.tfrecords_file_path, 'tran.tfrecords')
+    tfrecords_f = os.path.join(args.tfrecords_file_path, 'train.tfrecords')
     dataset = tf.data.TFRecordDataset(tfrecords_f)
     dataset = dataset.map(parse_function)
     dataset = dataset.shuffle(buffer_size=30000)
@@ -116,8 +120,10 @@ if __name__ == '__main__':
         while True:
             try:
                 images, labels = sess.run(next_element)
-                cv2.imshow('test', images[1, ...])
-                cv2.waitKey(0)
+                plt.imshow(images[1, ...])
+                plt.show()
+                # cv2.imshow('test', images[1, ...])
+                # cv2.waitKey(0)
             except tf.errors.OutOfRangeError:
                 print("End of dataset")
 
